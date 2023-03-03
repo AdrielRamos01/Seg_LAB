641a114409912faa473f2c70039788a99c702934
Adriel Ramos <adriel.ramos.ayuso@alumnos.upm.es>
2023-03-03 12:27:34 +0100

    Metodo generar claves 2.0

--- P1/proyecto/src/Asimetrico.java
+++ P1/proyecto/src/Asimetrico.java
@@ -1,8 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
+import org.bouncycastle.crypto.AsymmetricCipherKeyPair;
 import org.bouncycastle.crypto.generators.RSAKeyPairGenerator;
 import org.bouncycastle.crypto.params.RSAKeyGenerationParameters;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.util.encoders.Hex;
 
 public class Asimetrico {
 
@@ -17,6 +24,35 @@ public class Asimetrico {
 		//2. Instanciar generador de claves
 		RSAKeyPairGenerator generadorClaves = new RSAKeyPairGenerator();
 		generadorClaves.init(parametros);
+		
+		//3. Inicializarlo. Generar el par de claves necesario
+		AsymmetricCipherKeyPair claves = generadorClaves.generateKeyPair();
+		
+		
+		//4. Escritura de las claves en los ficheros correspondientes
+		try {
+			PrintWriter ficheroPrivada = new PrintWriter(new FileWriter(ficheroKPrivada));
+			PrintWriter ficheroPublica = new PrintWriter(new FileWriter(ficheroKPublica));
+			
+			RSAKeyParameters privada=(RSAKeyParameters) claves.getPublic();
+			RSAKeyParameters publica=(RSAKeyParameters) claves.getPublic();
+			
+			ficheroPrivada.println(new String (Hex.encode(privada.getModulus().toByteArray())));
+			ficheroPrivada.print(new String (Hex.encode(privada.getExponent().toByteArray())));
+			
+			ficheroPrivada.println(new String (Hex.encode(publica.getModulus().toByteArray())));
+			ficheroPrivada.print(new String (Hex.encode(publica.getExponent().toByteArray())));
+			
+			ficheroPrivada.close();
+			ficheroPublica.close();
+			
+			
+			} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			} catch (IOException e) {
+			e.printStackTrace();
+			}
+		
 		
 		
 	}
